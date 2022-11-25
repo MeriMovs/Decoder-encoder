f47712ba8bb1fc17da9a1ccfd71ad272b61fec23
MeriMovs <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>
2022-11-25 12:30:59 +0400

    last commit 25.11.2022

--- Animal/source.cpp
+++ Animal/source.cpp
@@ -7,164 +7,89 @@ class Animal
         int m_id;
         std::string m_Animal_sound;
         std::string m_Animal_feed;
+        std::string m_Animal_color;
+        std::string m_Animal_family;
 
     public:
-        virtual void toFeed(std::string) = 0;
-        virtual void makeSound(std::string) = 0;
+        void toFeed(std::string);
+        void makeSound(std::string);
+
+
+        void set_Animal_id(int);
+        int get_Animal_id();
+
+        void set_Animal_name(std::string);
+        std::string get_Animal_name();
+
+        void set_Animal_color(std::string);
+        std::string get_Animal_color();
+
+        void set_Animal_family(std::string);
+        std::string get_Animal_family();
 };
 
 class Dog : public Animal
 {
-    protected:
-        std::string m_Dog_color;
-        std::string m_Dog_family;
-
-    public:
-        void toFeed(std::string);
-        void makeSound(std::string);
-
-        void set_Dog_id(int);
-        int get_Dog_id();
-
-        void set_Dog_name(std::string);
-        std::string get_Dog_name();
-
-        void set_Dog_color(std::string);
-        std::string get_Dog_color();
-
-        void set_Dog_family(std::string);
-        std::string get_Dog_family();
 
 };
-
-
-void Dog::toFeed(std::string feed){
-            m_Animal_feed = feed;
-            std::cout << "Dog to feed "<< m_Animal_feed << std::endl;
-        }
-void Dog::makeSound(std::string sound){
-            m_Animal_sound = sound;
-            std::cout << "Dog make sound " << m_Animal_sound << std::endl;
-        }
-void Dog::set_Dog_id(int id){
-            m_id = id;
-        }
-int Dog::get_Dog_id(){
-            std::cout << "Get Dog id: " << m_id << std::endl; 
-            return m_id;
-        }
-void Dog::set_Dog_name(std::string name){
-            m_name = name;
-        }
-std::string Dog:: get_Dog_name(){
-            std::cout << "Get Dog name: " << m_name << std::endl; 
-            return m_name;
-        }
-void Dog::set_Dog_color(std::string color){
-            m_Dog_color = color;
-        }
-std::string Dog:: get_Dog_color(){
-            std::cout << "Get Dog color: " << m_Dog_color << std::endl; 
-            return m_Dog_color;
-        }
-void Dog::set_Dog_family(std::string family){
-            m_Dog_family = family;
-        }
-std::string Dog::get_Dog_family(){
-            std::cout << "Get Dog family: " << m_Dog_family << std::endl; 
-            return m_Dog_family;
-        }
-
-
-
-
-
-
-
-
 class Cat : public Animal
 {
-    protected:
-        std::string m_Cat_color;
-        std::string m_Cat_family;
-
-    public:
-        void toFeed(std::string);
-        void makeSound(std::string);
-
-        void set_Cat_id(int);
-        int get_Cat_id();
-
-        void set_Cat_name(std::string);
-        std::string get_Cat_name();
-
-        void set_Cat_color(std::string);
-        std::string get_Cat_color();
-
-        void set_Cat_family(std::string);
-        std::string get_Cat_family();
 
 };
 
-
-void Cat::toFeed(std::string feed){
+void Animal::toFeed(std::string feed){
             m_Animal_feed = feed;
-            std::cout << "Cat to feed "<< m_Animal_feed << std::endl;
+            std::cout << "Animal to feed "<< m_Animal_feed << std::endl;
         }
-void Cat::makeSound(std::string sound){
+void Animal::makeSound(std::string sound){
             m_Animal_sound = sound;
-            std::cout << "Cat make sound " << m_Animal_sound << std::endl;
+            std::cout << "Animal make sound " << m_Animal_sound << std::endl;
         }
-void Cat::set_Cat_id(int id){
+void Animal::set_Animal_id(int id){
             m_id = id;
         }
-int Cat::get_Cat_id(){
-            std::cout << "Get Cat id: " << m_id << std::endl; 
+int Animal::get_Animal_id(){
+            std::cout << "Get Animal id: " << m_id << std::endl; 
             return m_id;
         }
-void Cat::set_Cat_name(std::string name){
+void Animal::set_Animal_name(std::string name){
             m_name = name;
         }
-std::string Cat:: get_Cat_name(){
-            std::cout << "Get Cat name: " << m_name << std::endl; 
+std::string Animal:: get_Animal_name(){
+            std::cout << "Get Animal name: " << m_name << std::endl; 
             return m_name;
         }
-void Cat::set_Cat_color(std::string color){
-            m_Cat_color = color;
+void Animal::set_Animal_color(std::string color){
+            m_Animal_color = color;
         }
-std::string Cat:: get_Cat_color(){
-            std::cout << "Get Cat color: " << m_Cat_color << std::endl; 
-            return m_Cat_color;
+std::string Animal:: get_Animal_color(){
+            std::cout << "Get Animal color: " << m_Animal_color << std::endl; 
+            return m_Animal_color;
         }
-void Cat::set_Cat_family(std::string family){
-            m_Cat_family = family;
+void Animal::set_Animal_family(std::string family){
+            m_Animal_family = family;
         }
-std::string Cat::get_Cat_family(){
-            std::cout << "Get Cat family: " << m_Cat_family << std::endl; 
-            return m_Cat_family;
+std::string Animal::get_Animal_family(){
+            std::cout << "Get Animal family: " << m_Animal_family << std::endl; 
+            return m_Animal_family;
         }
-
-
-
-
-
 
 int main(){
     Dog Sharik;
     Sharik.toFeed("flower");
     Sharik.makeSound("haf-haf");
 
-    Sharik.set_Dog_id(5);
-    Sharik.get_Dog_id();
+    Sharik.set_Animal_id(5);
+    Sharik.get_Animal_id();
 
-    Sharik.set_Dog_name("Jack");
-    Sharik.get_Dog_name();
+    Sharik.set_Animal_name("Jack");
+    Sharik.get_Animal_name();
 
-    Sharik.set_Dog_color("brown");
-    Sharik.get_Dog_color();
+    Sharik.set_Animal_color("brown");
+    Sharik.get_Animal_color();
 
-    Sharik.set_Dog_family("retriever");
-    Sharik.get_Dog_family();
+    Sharik.set_Animal_family("retriever");
+    Sharik.get_Animal_family();
 
 
     std::cout << "\n\n";
@@ -173,16 +98,16 @@ int main(){
     Murzik.toFeed("nerves");
     Murzik.makeSound("myau");
 
-    Murzik.set_Cat_id(2);
-    Murzik.get_Cat_id();
+    Murzik.set_Animal_id(2);
+    Murzik.get_Animal_id();
 
-    Murzik.set_Cat_name("Murzik");
-    Murzik.get_Cat_name();
+    Murzik.set_Animal_name("Murzik");
+    Murzik.get_Animal_name();
 
-    Murzik.set_Cat_color("gray");
-    Murzik.get_Cat_color();
+    Murzik.set_Animal_color("gray");
+    Murzik.get_Animal_color();
 
-    Murzik.set_Cat_family("leopard");
-    Murzik.get_Cat_family();
+    Murzik.set_Animal_family("leopard");
+    Murzik.get_Animal_family();
 
 }
